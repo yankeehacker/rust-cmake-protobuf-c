8f3ac6d906c790a562fecc973e61faf8b5aeecd1
Nathan Cooprider <nathan.cooprider@stresearch.com>
2022-06-08 15:26:13 -0400

    This should have failed. . . but it didn't.

--- rusty-bits/build.rs
+++ rusty-bits/build.rs
@@ -2,5 +2,11 @@ extern crate cmake;
 use cmake::Config;
 
 fn main() {
+    let _protobuf_dst = Config::new("../deps/protobuf/cmake")
+        .define("protobuf_BUILD_TESTS","OFF")
+        .build();
+    let _protobuf_c_dst = Config::new("../deps/protobuf-c/build-cmake")
+        .cxxflag("-std=c++11")
+        .build();
 
 }
